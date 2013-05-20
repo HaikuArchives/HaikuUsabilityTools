6e1dbe828995a8c6bfde0d32c677d11e3dc28122
tdreed@gmail.com <tdreed@gmail.com>
2013-05-20 19:13:02 +0000

    Added icon

--- run/MainWindow.h
+++ run/MainWindow.h
@@ -1,16 +1,28 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
-#include <Window.h>
+#include "IconView.h"
+
+#include <InterfaceKit.h>
 
 class MainWindow : public BWindow
 {
 public:
 						MainWindow(void);
+			virtual		~MainWindow();
 			void		MessageReceived(BMessage *msg);
 			bool		QuitRequested(void);
 			
 private:
+			BButton*		fRunButton;
+			BButton*		fBrowseButton;			
+			IconView*		fIconView;
+			BTextControl*	fTargetText;
+			BFilePanel*		fTargetPanel;
+			BCheckBox*		fUseTerminal;
+			
+			status_t		_OpenFile(const char* openWith, BEntry &entry, int32 line = -1, int32 col = -1);
+			int				_Launch();
 };
 
 #endif
